968b0bbe1e45edff324c7da34629bbed7f47a52b
Eduardo Costa <m4c0@users.noreply.github.com>
2023-05-18 11:29:00 +0100

    using a visual output with multiple "components"

--- poc.cpp
+++ poc.cpp
@@ -14,6 +14,10 @@ struct rigid_body {
   body_type type;
 };
 
+struct lifeness {
+  bool alive;
+};
+
 class coord {
   unsigned m_x;
   unsigned m_y;
@@ -33,7 +37,9 @@ int main() {
   srand(69);
 
   ecs::sparse_set<rigid_body, max> bodies{};
+  ecs::sparse_set<lifeness, max> alives{};
 
+  // Simulates bodies from a map
   for (auto x = 0U; x < width; x++) {
     bodies.add(coord{x, 0}, rigid_body{block});
     bodies.add(coord{x, height - 1}, rigid_body{block});
@@ -43,6 +49,7 @@ int main() {
     bodies.add(coord{width - 1, y}, rigid_body{block});
   }
 
+  // Simulates picking random position for new mobs
   for (auto i = 0; i < mob_count; i++) {
     coord c{};
     do {
@@ -50,9 +57,32 @@ int main() {
     } while (bodies.has(c));
 
     bodies.add(c, rigid_body{mob});
+    alives.add(c, lifeness{true});
   }
 
+  // Simulates mob movement
+  // All mob wants to go to 0, 0 and kills on contact
+  for (auto [alive, id] : alives) {
+    // attack target; or
+    // remove body
+    // add body
+  }
+
+  // Simulates output
+  char out[height * width];
+  for (auto &o : out)
+    o = ' ';
   for (auto [body, id] : bodies) {
-    printf("%d %d = %d\n", id % width, id / width, body.type);
+    if (body.type == block) {
+      out[id] = '#';
+    } else if (alives.has(id)) {
+      out[id] = '@';
+    } else {
+      out[id] = '*';
+    }
+  }
+  for (auto y = 0; y < height; y++) {
+    fwrite(out + y * width, 1, width, stdout);
+    puts("");
   }
 }
