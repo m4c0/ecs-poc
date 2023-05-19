712fc41f87ecc6f586148a8330239af2355b7bc4
Eduardo Costa <m4c0@users.noreply.github.com>
2023-05-19 11:54:45 +0100

    using the new eid in poc

--- poc.cpp
+++ poc.cpp
@@ -3,108 +3,126 @@
 
 import ecs;
 
+unsigned random(unsigned n) { return rand() % n; }
+
 constexpr const auto width = 10;
 constexpr const auto height = 5;
 constexpr const auto max = width * height;
 
 constexpr const auto mob_count = height;
 
+//------ "Components"
+
 enum body_type { block, mob };
 struct rigid_body {
   body_type type;
 };
-using bodies_set = ecs::sparse_set<rigid_body, max>;
 
 struct lifeness {
   bool alive;
 };
-using alives_set = ecs::sparse_set<lifeness, max>;
 
-class coord {
-  unsigned m_x;
-  unsigned m_y;
-
-public:
-  constexpr coord() = default;
-  constexpr coord(unsigned x, unsigned y) : m_x{x}, m_y{y} {}
-
-  explicit constexpr coord(unsigned i) : m_x{i % width}, m_y{i / width} {}
-
-  [[nodiscard]] constexpr operator unsigned() const noexcept {
-    return m_y * width + m_x;
-  }
-
-  constexpr void decr_y() noexcept { m_y--; }
+struct coord {
+  unsigned x;
+  unsigned y;
 };
 
-unsigned random(unsigned n) { return rand() % n; }
+struct ec {
+  ecs::entity_list<max> e;
+  ecs::sparse_set<coord, max> coords;
+  ecs::sparse_set<rigid_body, max> bodies;
+  ecs::sparse_set<lifeness, max> alives;
+  ecs::eid map[height][width];
+};
+
+//------ Entity creation
+
+void add_block(ec &ec, coord c) {
+  auto id = ec.e.alloc();
+  ec.coords.add(id, c);
+  ec.bodies.add(id, rigid_body{block});
+  ec.map[c.y][c.x] = id;
+}
+void add_mob(ec &ec, coord c) {
+  auto id = ec.e.alloc();
+  ec.coords.add(id, c);
+  ec.bodies.add(id, rigid_body{mob});
+  ec.alives.add(id, lifeness{true});
+  ec.map[c.y][c.x] = id;
+}
 
 //------ "Systems"
 
-void gen_map(bodies_set &bodies) {
+void gen_map(ec &ec) {
   // Simulates bodies from a map
   for (auto x = 0U; x < width; x++) {
-    bodies.add(coord{x, 0}, rigid_body{block});
-    bodies.add(coord{x, height - 1}, rigid_body{block});
+    add_block(ec, coord{x, 0});
+    add_block(ec, coord{x, height - 1});
   }
   for (auto y = 0U; y < height; y++) {
-    bodies.add(coord{0, y}, rigid_body{block});
-    bodies.add(coord{width - 1, y}, rigid_body{block});
+    add_block(ec, coord{0, y});
+    add_block(ec, coord{width - 1, y});
   }
 }
-void gen_mobs(bodies_set &bodies, alives_set &alives) {
+
+void gen_mobs(ec &ec) {
   // Simulates picking random position for new mobs
   for (auto i = 0; i < mob_count; i++) {
     coord c{};
     do {
       c = {random(width), random(height)};
-    } while (bodies.has(c));
+    } while (ec.map[c.y][c.x]);
 
-    bodies.add(c, rigid_body{mob});
-    alives.add(c, lifeness{true});
+    add_mob(ec, c);
   }
 }
 
-void move_mobs(const bodies_set &bodies, alives_set &alives) {
+void move_mobs(ec &ec) {
   // Simulates mob movement
   // All mob wants to go to up and kills on contact
-  for (auto [alive, id] : alives) {
-    coord tgt{id};
-    tgt.decr_y();
+  for (const auto [alive, id] : ec.alives) {
+    coord c = ec.coords.get(id);
+    ecs::eid tgt = ec.map[c.y - 1][c.x];
 
-    if (bodies.has(tgt) && alives.has(tgt)) {
-      alives.set(tgt, lifeness{false});
+    if (ec.bodies.has(tgt) && ec.alives.has(tgt)) {
+      ec.alives.set(tgt, lifeness{false});
       continue;
     }
     // add desired target
   }
 }
 
-void dead_cleanup(bodies_set &bodies, alives_set &alives) {
-  for (auto [a, id] : alives) {
-    if (!a.alive)
-      bodies.remove(id);
+void dead_cleanup(ec &ec) {
+  for (const auto [a, id] : ec.alives) {
+    if (a.alive)
+      continue;
+
+    auto c = ec.coords.get(id);
+    ec.map[c.y][c.x] = {};
+    ec.coords.remove(id);
+    ec.bodies.remove(id);
+    ec.e.dealloc(id);
   }
-  alives.remove_if([](auto l, auto id) { return !l.alive; });
+  ec.alives.remove_if([](auto l, auto id) { return !l.alive; });
 }
 
-void output(const bodies_set &bodies, const alives_set &alives) {
+void output(const ec &ec) {
   // Simulates output
-  char out[height * width];
-  for (auto &o : out)
-    o = ' ';
-  for (auto [body, id] : bodies) {
-    if (body.type == block) {
-      out[id] = '#';
-    } else if (alives.has(id)) {
-      out[id] = '@';
-    } else {
-      out[id] = '*';
+  for (auto &row : ec.map) {
+    char buf[width + 1];
+    for (auto i = 0; i < width; i++) {
+      auto id = row[i];
+      if (!id) {
+        buf[i] = ' ';
+      } else if (ec.bodies.get(id).type == block) {
+        buf[i] = '#';
+      } else if (ec.alives.has(id)) {
+        buf[i] = '@';
+      } else {
+        buf[i] = '*';
+      }
     }
-  }
-  for (auto y = 0; y < height; y++) {
-    fwrite(out + y * width, 1, width, stdout);
-    puts("");
+    puts(buf);
   }
 }
 
@@ -120,15 +138,14 @@ int main() {
   // One entity can't walk, three can, one will attack
   srand(69);
 
-  ecs::sparse_set<rigid_body, max> bodies{};
-  ecs::sparse_set<lifeness, max> alives{};
+  ec ec{};
 
-  gen_map(bodies);
-  gen_mobs(bodies, alives);
-  move_mobs(bodies, alives);
-  output(bodies, alives);
+  gen_map(ec);
+  gen_mobs(ec);
+  move_mobs(ec);
+  output(ec);
 
-  dead_cleanup(bodies, alives);
+  dead_cleanup(ec);
   // move to targets
-  output(bodies, alives);
+  output(ec);
 }
