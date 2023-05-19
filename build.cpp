28bd35b40aced5cc8099d445f109a80c85d26c8c
Eduardo Costa <m4c0@users.noreply.github.com>
2023-05-19 11:15:01 +0100

    moving eid to a part

--- build.cpp
+++ build.cpp
@@ -6,6 +6,7 @@ int main(int argc, char **argv) {
 
   auto m = unit::create<mod>("ecs");
   m->add_wsdep("hai", hai());
+  m->add_part("eid");
   m->add_part("spset");
 
   auto poc = unit::create<tool>("poc");
