fca769ca55e239b22a43c224500d778ff64876d3
Eduardo Costa <m4c0@users.noreply.github.com>
2023-08-05 16:20:52 +0100

    build: using box to autodetect deps

--- build.hpp
+++ build.hpp
@@ -1,18 +1,12 @@
 #pragma once
 
-#include "../btree/build.hpp"
 #include "../ecow/ecow.hpp"
-#include "../hai/build.hpp"
-#include "../rtree/build.hpp"
-#include "../traits/build.hpp"
+#include "../silog/build.hpp"
 
 auto pog() {
   using namespace ecow;
 
-  auto m = unit::create<mod>("pog");
-  m->add_wsdep("hai", hai());
-  m->add_wsdep("btree", btree());
-  m->add_wsdep("rtree", rtree());
-  m->add_wsdep("traits", traits());
+  auto m = unit::create<box>("pog");
+  m->add_wsdep("silog", silog());
   return m;
 }
